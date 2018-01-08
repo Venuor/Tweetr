d4ef34af180686db498b7f6d34e833d4362e5108
rspiess <rouvenspiess@gmx.de>
2018-01-08 17:30:05 +0100

    added endpoint for user timeline

--- app/api/tweets.js
+++ app/api/tweets.js
@@ -3,6 +3,7 @@
 const UserController = require('../controller/users');
 const TweetController = require('../controller/tweets');
 const ObjectUtil = require('../util/objectutil');
+const JwtUtil = require('../util/jwtutil');
 const Boom = require('boom');
 
 exports.findTweets = {
@@ -27,6 +28,21 @@ exports.allTweets = {
           reply(ObjectUtil.getTweetsArray(tweets)).code(200);
         }).catch(err => {
           reply(Boom.badImplementation('error accessing db'));
+        });
+  },
+};
+
+exports.timeline = {
+  auth: 'jwt',
+  handler: function (request, reply) {
+    const info = JwtUtil.decodeToken(request.headers.authorization);
+    UserController.getUser(info.username)
+        .then(user => {
+          return TweetController.getSubscribedTweets(user);
+        }).then(tweets => {
+          reply(ObjectUtil.getTweetsArray(tweets)).code(200);
+        }).catch(err => {
+          reply(Boom.badRequest('User not found'));
         });
   },
 };
